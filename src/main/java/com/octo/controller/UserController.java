4ab3524de2fd8bd5038694d82349a717091eb480
zms <octoz4ms@gmail.com>
2025-03-25 17:43:17 +0800

    fix: Merge code

--- src/main/java/com/octo/controller/UserController.java
+++ src/main/java/com/octo/controller/UserController.java
@@ -1,18 +1,11 @@
 package com.octo.controller;
 
 
+import com.octo.entity.User;
 import com.octo.service.IUserService;
 import com.octo.util.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import com.octo.entity.User;
-import com.octo.util.ApiResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -27,12 +20,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private IUserService userService;
+
     @GetMapping("/{id}")
     public ApiResponse<User> getUser(@PathVariable("id") int id) {
         return ApiResponse.success();
     }
-    @Autowired
-    private IUserService userService;
 
     @PostMapping("/import/excel")
     public ApiResponse<?> importExcel(@RequestParam("file") MultipartFile file) {
